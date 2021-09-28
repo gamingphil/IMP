7e83dcb67f70e7cf46759519c52da753880e5300
Phil M <83034452+gamingphil@users.noreply.github.com>
2021-09-28 19:30:16 +0200

    for loop fix
    for loop fixed +
    random number generator integers,
    length as function parameter

--- Arrays/Umgang mit Arrays.js
+++ Arrays/Umgang mit Arrays.js
@@ -1,25 +1,24 @@
 function getZufallszahl(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.ceil(Math.random() * (max - min) + min);
   }
 
-function erzeugeZufallsarray () {
+function erzeugeZufallsarray (LEN) {
 
-    let laenge = 4
+    let laenge = LEN;
     if(isNaN(laenge)) return console.log("laenge muss eine Zahl sein");
-    if(Math.sign(laenge) === -1 || Math.sign(laenge) === 0) return console.log("Zahl muss größer als 0 sein")
-    let zahlen = new Array(laenge);
-    zahlen[0] = 20;
-    zahlen[1] = 50;
-    zahlen[2] = 30;
-    zahlen[3] = 100;
+    if(Math.sign(laenge) === -1 || Math.sign(laenge) === 0) return console.log("Zahl muss größer als 0 sein");
+    var zahlen = new Array(laenge);
+    // zahlen[0] = 20;
+    // zahlen[1] = 50;
+    // zahlen[2] = 30;
+    // zahlen[3] = 100;
 
 
-    for(let i = 0; i > zahlen.length; i++) {
+    for(let i = 0; i < zahlen.length; i++) {
         zahlen[i] = getZufallszahl(1, 300);
     }
 
     return zahlen;
-
 }
 
-console.log(erzeugeZufallsarray());
+console.log(erzeugeZufallsarray(4));
