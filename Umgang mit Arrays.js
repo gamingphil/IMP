12ef2120136190c9bde91a2c1bdf33a454deae79
Phil M <83034452+gamingphil@users.noreply.github.com>
2021-09-29 09:33:50 +0200

    ich bin dumm jetzt hab ich nicht zwei variablen mehr

--- Umgang mit Arrays.js
+++ Umgang mit Arrays.js
@@ -4,10 +4,9 @@ function getZufallszahl(min, max) {
 
 function erzeugeZufallsarray (LEN) {
 
-    let laenge = LEN;
-    if(isNaN(laenge)) return console.log("laenge muss eine Zahl sein");
-    if(Math.sign(laenge) === -1 || Math.sign(laenge) === 0) return console.log("Zahl muss größer als 0 sein");
-    var zahlen = new Array(laenge);
+    if(isNaN(LEN)) return console.log("laenge muss eine Zahl sein");
+    if(Math.sign(LEN) === -1 || Math.sign(LEN) === 0) return console.log("Zahl muss größer als 0 sein");
+    var zahlen = new Array(LEN);
     // zahlen[0] = 20;
     // zahlen[1] = 50;
     // zahlen[2] = 30;
