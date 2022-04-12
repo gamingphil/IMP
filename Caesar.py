d009045c80c93808ba07f7daf57a567e33d9c39e
Phil M <83034452+gamingphil@users.noreply.github.com>
2022-04-13 00:33:52 +0200

    conversion fixed

--- Caesar.py
+++ Caesar.py
@@ -1,25 +1,27 @@
-input = "NMAB OMUICMZB QV LMZ MZLMV ABMPB LQM NWZU ICA TMPU OMJZIVVB PMCBM UCAA LQM OTWKSM EMZLMV NZQAKP OMAMTTMV AMQL HCZ PIVL DWV LMZ ABQZVM PMQAA ZQVVMV UCAA LMZ AKPEMQAA AWTT LIA EMZS LMV UMQABMZ TWJMV LWKP LMZ AMOMV SWUUB DWV WJMV"
+impischdoof = "NMAB OMUICMZB QV LMZ MZLMV ABMPB LQM NWZU ICA TMPU OMJZIVVB PMCBM UCAA LQM OTWKSM EMZLMV NZQAKP OMAMTTMV AMQL HCZ PIVL DWV LMZ ABQZVM PMQAA ZQVVMV UCAA LMZ AKPEMQAA AWTT LIA EMZS LMV UMQABMZ TWJMV LWKP LMZ AMOMV SWUUB DWV WJMV"
 
-def letterFreq(string):
-    # toUpperCase
-    lArray = []
-    fArray = []
-    for i in range(26):  
-        fArray.append(0)    
-    for i in string:
+def caesar(input):
+    frequency = []
+    for i in range(26):
+        frequency.append(0)
+    letters = []
+    for i in input.upper():
         if not i == " ":
-            lArray.append(i)
-    for i in lArray:
-        fArray[ord(i)-65] = fArray[ord(i)-65] + 1
-    return fArray.index(max(fArray))
+            letters.append(i)
+    for i in letters:
+        frequency[ord(i)-65] += 1
+    shift = frequency.index(max(frequency)) - 4
+    output = ""
+    for i in input.upper():
+        if i == " " :
+            output = output + str(i)
+        else:
+            shiftedORD = ord(i) - shift
+            if shiftedORD < 65:
+                shiftedORD = 91 - (65 - shiftedORD)
+            elif shiftedORD > 90:
+                shiftedORD = 64 + (shiftedORD - 90)
+            output = output + str(chr(shiftedORD))
+    return output
 
-output = ""
-for i in input:
-    print("i: "+ i)
-if i == " ":
-    output += output + " "
-else:
-    output += output + chr(ord(i)+letterFreq(input))
-    print("currentOutput: " + output)
-
-print(output)
+print(caesar(impischdoof))
